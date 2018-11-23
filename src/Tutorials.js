61512d30d37642fc6a9dba95c917804964fb7ff6
Gareth <gareth.clifford@live.co.uk>
2018-11-23 13:14:13 +0000

    Resizing

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -38,7 +38,7 @@ class Tutorials extends Component {
 
   tutorialVideo = (cell, row) => {
     var tutorialURL = this.state.tuts[cell-1].tutorialLink;
-    this.setState({cellno:row.tutorialID.value});
+    this.setState({cellno:this.state.tuts.tutorialID.value});
     console.log(this.state.cellno);
     console.log(this.state.tuts[cell-1].tutorialLink);
     ReactDOM.render(<Videothing videourl={this.state.tuts[cell-1].tutorialLink} />,document.getElementById('seeVideo'));
