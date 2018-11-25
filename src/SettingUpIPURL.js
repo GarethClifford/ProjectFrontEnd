2c7f6d2f8391e075ca587f6c9d6634feeee93e0d
garethclifford <gareth.clifford@live.co.uk>
2018-11-25 15:41:44 +0000

    Refactor to simplify axios requests

--- src/SettingUpIPURL.js
+++ src/SettingUpIPURL.js
@@ -1,6 +1,6 @@
 const port ='8080';
-const ipAddress ='http://104.199.39.236:' + port;
+const ipAddress ='http://104.199.39.236:' + port + '/';
 const warFile ='MyPersonalProject/rest/';
 
 
-export const root = ipAddress + warFile
+export const root = ipAddress + warFile;
